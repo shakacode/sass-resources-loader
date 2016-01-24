ede0223ddf9d0717371bbafec24f7a2c4fc9af5c
Alex Fedoseev <alex.fedoseev@gmail.com>
2016-01-24 19:31:18 +0300

    Fix rebuild issue

---
+++ src/utils/processResources.js
@@ -0,0 +1,18 @@
+import logger from './logger';
+
+export default function(error, resources, source, module, callback) {
+  if (error) {
+    logger.debug('Resources: **not found**');
+    return callback(error);
+  }
+
+  const stringifiedResources = (
+    Array.isArray(resources) ? resources.join('\n') : resources + '\n'
+  );
+
+  const output = stringifiedResources + source;
+
+  logger.debug('Resources: \n', `/* ${module} */ \n`, output);
+
+  return callback(null, output);
+}
