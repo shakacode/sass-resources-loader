c367f4f4caf2945893d4baa97266326ac882ea60
Nicholas Rawlings <nicholas.rawlings@pointsource.com>
2017-05-08 16:57:19 -0400

    Rewrite @import paths in resources so that Sass can resolve the imports

---
+++ src/utils/rewriteImports.js
@@ -0,0 +1,27 @@
+import path from 'path';
+
+import logger from './logger';
+
+const importRegexp = /@import\s+(?:'([^']+)'|"([^"]+)"|([^\s;]+))/g;
+
+export default (error, file, contents, moduleContext, callback) => {
+  if (error) {
+    logger.debug('Resources: **not found**');
+    return callback(error);
+  }
+
+  const rewritten = contents.replace(importRegexp, (entire, single, double, unquoted) => {
+    const oldImportPath = single || double || unquoted;
+    const absoluteImportPath = path.join(path.dirname(file), oldImportPath);
+    const newImportPath = path.relative(moduleContext, absoluteImportPath);
+
+    logger.debug(`Resources: @import of ${oldImportPath} changed to ${newImportPath}`);
+
+    const lastCharacter = entire[entire.length - 1];
+    const quote = lastCharacter === "'" || lastCharacter === '"' ? lastCharacter : '';
+
+    return `@import ${quote}${newImportPath}${quote}`;
+  });
+
+  callback(null, rewritten);
+};
