5ef7d8b619a3fa5e974f43a55140b193b1417d15
alexeyMohnatkin <dizzymohnatkin@gmail.com>
2017-07-29 00:07:00 +0300

    handle correctly @import starting with ~

--- src/utils/rewriteImports.js
+++ src/utils/rewriteImports.js
@@ -14,14 +14,24 @@ export default (error, file, contents, moduleContext, rootContext, callback) => 
     return callback(null, contents);
   }
 
+  const getNewImportPath = (oldImportPath) => {
+    const absoluteImportPath = path.join(path.dirname(file), oldImportPath);
+
+    // if import path starts with / resolve from project root or webpack.context
+    if ((/^\//).test(oldImportPath)) {
+      return path.join(rootContext, oldImportPath);
+    }
+    // from node_modules
+    if ((/^\~/).test(oldImportPath)) {
+      return oldImportPath;
+    }
+    return path.relative(moduleContext, absoluteImportPath);
+  };
+
   const rewritten = contents.replace(importRegexp, (entire, single, double, unquoted) => {
     const oldImportPath = single || double || unquoted;
 
-    // if import path starts with / resolve from project root or webpack.context
-    const fromRoot = (/^\//).test(oldImportPath);
-    const absoluteImportPath = path.join(path.dirname(file), oldImportPath);
-    const newImportPath = fromRoot ? path.join(rootContext, oldImportPath) : path.relative(moduleContext, absoluteImportPath);
-
+    const newImportPath = getNewImportPath(oldImportPath);
     logger.debug(`Resources: @import of ${oldImportPath} changed to ${newImportPath}`);
 
     const lastCharacter = entire[entire.length - 1];
